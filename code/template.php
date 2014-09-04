3de8851173da7f5e0d39e46ac7289eab3832f483
markos <markos@joomlashack.com>
2014-09-04 10:10:22 -0600

    adding mediun weight to Roboto font

--- code/template.php
+++ code/template.php
@@ -19,7 +19,7 @@ defined('_JEXEC') or die('Restricted access');
 <html>
     <head>
         <w:head />
-        <link href='http://fonts.googleapis.com/css?family=Roboto:400,700' rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=Roboto:400,500,700' rel='stylesheet' type='text/css'>
         <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700' rel='stylesheet' type='text/css'>
     </head>
     <body class="<?php echo $responsive . $sidebar2_exist; ?>">
