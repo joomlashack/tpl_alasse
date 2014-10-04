99cd0a148585401a3500afedb4cccd3865413dde
jmarkos <markos@joomlashack.com>
2014-10-04 16:57:58 -0600

    adding fix of blog to featured blog

--- code/html/com_content/featured/default.php
+++ code/html/com_content/featured/default.php
@@ -12,12 +12,33 @@ defined('_JEXEC') or die;
 
 $app = JFactory::getApplication();
 $template = $app->getTemplate(true);
+$input = $app->input;
 
 // Template's gridMode
-$this->wrightNonContentContainer = ($template->params->get('bs_rowmode', 'row') == 'row' ? 'container' : 'container-fluid');
 $this->wrightNonContentRowMode = 'row-fluid';
-$this->wrightContentExtraContainer = ($template->params->get('bs_rowmode', 'row') == 'row' ? 'container' : 'container-fluid');
 $this->wrightImagesRow = true;
+
+// Check if there's a sidebar at all
+$sidebarExists = (JModuleHelper::getModules('sidebar1') || JModuleHelper::getModules('sidebar2'));
+// Use full width only if it's a blog or featured articles view
+$paramOption = $input->getVar('option', '');
+$paramView = $input->getVar('view', '');
+$paramLayout = $input->getVar('layout', 'default');
+$paramItemid = $input->getVar('Itemid', '');
+$paramId = $input->getVar('id', '');
+
+$alasseFullWidthBg = ($paramOption == 'com_content' && ($paramView == 'category' && $paramLayout == 'blog') || ($paramView == 'featured'));
+
+if ($alasseFullWidthBg && !$sidebarExists)
+{
+	$this->wrightNonContentContainer = ($template->params->get('bs_rowmode', 'row') == 'row' ? 'container' : 'container-fluid');
+	$this->wrightContentExtraContainer = ($template->params->get('bs_rowmode', 'row') == 'row' ? 'container' : 'container-fluid');
+}
+else
+{
+	$this->wrightNonContentContainer = 'container-fluid';
+    $this->wrightContentExtraContainer = 'container-fluid no-padding';
+}
 
 $this->wrightLeadingItemElementsStructure = Array(
 	'div.fullwidthimage',
@@ -39,6 +60,14 @@ $this->wrightLeadingItemElementsStructure = Array(
 			'/div',
 		'/div',
 	'/div'
+);
+
+$this->wrightIntroItemElementsStructure = Array(
+	'image',
+	'title',
+	'article-info',
+	'icons',
+	'content'
 );
 
 require_once JPATH_THEMES . '/' . $app->getTemplate() . '/wright/html/overrider.php';
